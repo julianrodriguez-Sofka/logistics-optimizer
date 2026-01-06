f624a2c958ae9ffc51b9ee2bb52fb0e7fb3485d6
J-Ciro <jciro.1984@gmail.com>
2026-01-06 14:37:26 -0500

    fix(back): fixed an issue with the package file being unable to run the test

---
+++ logistics-back/jest.config.js
@@ -0,0 +1,24 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.test.ts',
+    '!src/**/__tests__/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
+};
