cacc96b6e9299349368a024d26cacaed622b1d48
J-Ciro <jciro.1984@gmail.com>
2026-01-09 14:11:08 -0500

    feat(ProviderStatusAdapter): add ProviderStatusType to imports

--- logistics-front/src/utils/adapters/ProviderStatusAdapter.ts
+++ logistics-front/src/utils/adapters/ProviderStatusAdapter.ts
@@ -1,4 +1,4 @@
-import type { ISystemStatus, ISystemStatusMetrics, SystemStatusType } from '../../models/ProviderStatus';
+import type { ISystemStatus, ISystemStatusMetrics, SystemStatusType, ProviderStatusType } from '../../models/ProviderStatus';
 
 /**
  * Adapter to convert API response to ISystemStatus
