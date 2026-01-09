22e06881c3acaead702b6250fd5000e27a4aa3b3
J-Ciro <jciro.1984@gmail.com>
2026-01-09 14:28:22 -0500

    chore(sonarqube): created the conf for sonarqube and coverage

--- logistics-front/vite.config.ts
+++ logistics-front/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8', // o 'istanbul'
+      reporter: ['text', 'lcov', 'html'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.spec.ts',
+        '**/*.test.ts',
+        '**/*.test.tsx'
+      ]
+    }
+  }
 })
